cff0d4cb457fc71dea8d76b2eff310ef31890d4b
Valkirin <valkirinn@gmail.com>
2020-07-20 22:54:51 +0300

    initial commit

---
+++ scripts/fade-slider.js
@@ -0,0 +1,16 @@
+(function () {
+  const slides = document.querySelectorAll('.fade-slider__item');
+  const activeClass = 'fade-slider__item--visible';
+  let index = 0;
+
+  setInterval(() => {
+    slides[index].classList.remove(activeClass);
+    index++;
+
+    if (index + 1 > slides.length) {
+      index = 0;
+    } else {
+    }
+    slides[index].classList.add(activeClass);
+  }, 5000);
+})();
